7c734b608b5d8bf1784a28664eed16889143c773
sennzai <107695658+esutarosa@users.noreply.github.com>
2024-11-30 22:43:13 +0300

    update(nvim): colorscheme.lua

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,3 @@
--- return {
---   {
---     "sainnhe/sonokai",
---     priority = 1000,
---     config = function()
---       vim.g.sonokai_transparent_background = "1"
---       vim.g.sonokai_enable_italic = "1"
---       vim.g.sonokai_style = "andromeda"
---       vim.cmd.colorscheme("sonokai")
---     end,
---   },
--- }
-
 return {
   {
     "rose-pine/neovim",
@@ -18,11 +5,8 @@ return {
     priority = 1000,
     config = function()
       require("rose-pine").setup({
-        dark_variant = "main", -- или "moon"/"dawn" в зависимости от предпочтений
-        disable_background = false, -- Если нужен прозрачный фон, установите true
-        highlight_groups = {
-          -- Здесь можно настроить дополнительные группы, если необходимо
-        },
+        dark_variant = "main",
+        disable_background = false,
       })
       vim.cmd.colorscheme("rose-pine")
     end,
