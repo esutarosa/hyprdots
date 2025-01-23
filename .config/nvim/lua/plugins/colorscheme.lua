22d4ec28697662e0943e56069cf09675e4e5497c
ヒヤシンス <107695658+esutarosa@users.noreply.github.com>
2025-01-24 01:00:38 +0200

    update(nvim): colorscheme.lua

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,50 @@
-vim.cmd.colorscheme("catppuccin")
-
-require("catppuccin").setup({
-  flavour = "mocha",
-  background = {
-    light = "latte",
-    dark = "mocha",
-  },
-  integrations = {
-    nvimtree = true,
-    treesitter = true,
-    lualine = true,
-    telescope = true,
-    mason = true,
-    notify = false,
-    mini = {
-      enabled = true,
-      indentscope_color = "",
+return {
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      transparent_background = true,
+      integrations = {
+        aerial = true,
+        alpha = true,
+        cmp = true,
+        dashboard = true,
+        flash = true,
+        gitsigns = true,
+        headlines = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        leap = true,
+        lsp_trouble = true,
+        mason = true,
+        markdown = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true, custom_bg = "lualine" },
+        neotest = true,
+        neotree = true,
+        noice = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        treesitter_context = true,
+        which_key = true,
+      },
     },
   },
-  transparent_background = false,
-  term_colors = true,
-  compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-})
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
